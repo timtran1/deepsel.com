c7c1f2c9ec3918e7271bceef894f76144ed194c4
Tim Tran <tim.tran.201@gmail.com>
2023-04-14 16:50:40 +0700

    update

--- src/components/home/Blog.jsx
+++ src/components/home/Blog.jsx
@@ -1,4 +1,29 @@
+import {useEffect, useState} from 'react';
+
 export default function () {
+    const ghostContentApiKey = '2e8f07f1755eae40316d990073';
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetchLatestPosts();
+    }, []);
+
+    async function fetchLatestPosts() {
+        const apiEndpoint = `https://async-tech.co/blog/ghost/api/v3/content/posts/?key=${ghostContentApiKey}&limit=3`;
+
+        try {
+            const response = await fetch(apiEndpoint);
+            const data = await response.json();
+            const posts = data.posts;
+
+            // Do something with the posts array
+            console.log(posts);
+            setPosts(posts);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     return (
         <section className="news-section">
             <div className="auto-container">
@@ -9,81 +34,31 @@ export default function () {
                 </div>
 
                 <div className="row clearfix">
-
-
-                    <div className="news-block col-lg-4 col-md-6 col-sm-12">
-                        <div className="inner-box wow fadeInLeft" data-wow-delay="0ms" data-wow-duration="1500ms">
-                            <div className="image">
-                                <a href="blog-single.html"><img src="/images/resource/news-1.jpg" alt=""/></a>
-                            </div>
-                            <div className="lower-content">
-                                <h6><a href="blog-single.html">Top aide to possible contender forced to resign over
-                                    creepy.</a></h6>
-                                <div className="clearfix">
-                                    <div className="pull-left">
-                                        <div className="author">
-                                            <div className="image"><img src="/images/resource/author-4.jpg" alt=""/>
+                    {posts.map(post =>
+                        <div className="news-block col-lg-4 col-md-6 col-sm-12" key={post.id}>
+                            <div className="inner-box wow fadeInLeft" data-wow-delay="0ms" data-wow-duration="1500ms">
+                                <div className="image">
+                                    <a href={post.url}><img src={post.feature_image} alt=""/></a>
+                                </div>
+                                <div className="lower-content">
+                                    <h6><a href={post.url}>{post.title}</a></h6>
+                                    <div className="clearfix">
+                                        <div className="pull-left">
+                                            <div className="author">
+                                                <div className="image">
+                                                    <img src="/images/resource/author-4.jpg" alt=""/>
+                                                </div>
+                                                Rio Smith
                                             </div>
-                                            Rio Smith
                                         </div>
-                                    </div>
-                                    <div className="pull-right">
-                                        <div className="post-time">10 hours ago</div>
+                                        <div className="pull-right">
+                                            <div className="post-time">{post.reading_time} min read</div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-
-
-                    <div className="news-block col-lg-4 col-md-6 col-sm-12">
-                        <div className="inner-box wow fadeInLeft" data-wow-delay="300ms" data-wow-duration="1500ms">
-                            <div className="image">
-                                <a href="blog-single.html"><img src="/images/resource/news-2.jpg" alt=""/></a>
-                            </div>
-                            <div className="lower-content">
-                                <h6><a href="blog-single.html">Thirty-two surrogate mothers charged with human
-                                    trafficking.</a></h6>
-                                <div className="clearfix">
-                                    <div className="pull-left">
-                                        <div className="author">
-                                            <div className="image"><img src="/images/resource/author-5.jpg" alt=""/>
-                                            </div>
-                                            Jhonny Rip
-                                        </div>
-                                    </div>
-                                    <div className="pull-right">
-                                        <div className="post-time">5 hours ago</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-
-                    <div className="news-block col-lg-4 col-md-6 col-sm-12">
-                        <div className="inner-box wow fadeInLeft" data-wow-delay="600ms" data-wow-duration="1500ms">
-                            <div className="image">
-                                <a href="blog-single.html"><img src="/images/resource/news-3.jpg" alt=""/></a>
-                            </div>
-                            <div className="lower-content">
-                                <h6><a href="blog-single.html">Chinese clients have been released after agreeing to
-                                    keep.</a></h6>
-                                <div className="clearfix">
-                                    <div className="pull-left">
-                                        <div className="author">
-                                            <div className="image"><img src="/images/resource/author-6.jpg" alt=""/>
-                                            </div>
-                                            Mong Wanzhou
-                                        </div>
-                                    </div>
-                                    <div className="pull-right">
-                                        <div className="post-time">12 hours ago</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    )}
 
                 </div>
             </div>
