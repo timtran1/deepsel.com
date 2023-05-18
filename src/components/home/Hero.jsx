dededc20618ee3d6f9172504e7dce84873919922
Tim Tran <tim.tran.201@gmail.com>
2023-05-18 17:13:05 +0700

    update

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -4,7 +4,7 @@ import {faArrowRight} from "@fortawesome/free-solid-svg-icons";
 
 export default function () {
     return (
-        <section className="banner-section-two">
+        <section className="banner-section-two overflow-x-clip">
             <div className="image-layer" style={{backgroundImage: `url(/img/5.png)`}}></div>
             <div className="auto-container">
                 <div className="clearfix">
