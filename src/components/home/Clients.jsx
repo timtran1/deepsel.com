67b199f6f83d5e6377ef497d5a4ee9ed359cf314
mikiri <mikiri@mkr.local>
2023-10-02 18:06:01 +0700

    fix

--- src/components/home/Clients.jsx
+++ src/components/home/Clients.jsx
@@ -35,18 +35,17 @@ export default function () {
         centerPadding: "20px",
         autoPlay: true,
         prevArrow:
-         <div className={`flex items-center`}>
-                <div className="hidden md:block cursor-pointer !-top-[13px] absolute text-2xl text-black">
-                    <FontAwesomeIcon icon={faChevronLeft}/>
+
+                <div className=" cursor-pointer !inline-block w-[30px] h-[30px] max-sm:w-[70%] !-top-[13px] absolute text-2xl text-black">
+                    <FontAwesomeIcon icon={faChevronLeft} style={{color: "#000000", height: '30px', width: '30px', position: 'absolute', translate: '-50% -40%'}}/>
                 </div>
-         </div>
         ,
         nextArrow:
-            <div className={`flex items-center`}>
-                <div className="hidden md:block cursor-pointer !-top-[13px] absolute text-2xl text-black">
-                    <FontAwesomeIcon icon={faChevronRight}/>
+
+                <div className=" block cursor-pointer !inline-block max-sm:w-[70%] w-[30px] h-[30px] !-top-[13px] absolute text-2xl text-black">
+                    <FontAwesomeIcon icon={faChevronRight} style={{color: "#000000", height: '30px', width: '30px', position: 'absolute', translate: '-80% -40%'}}/>
                 </div>
-            </div>
+
     };
     return (
 
@@ -56,49 +55,49 @@ export default function () {
                 <h2>Our clients</h2>
             </div>
             <Slider className={'w-[80%] mx-auto'} {...settings}>
-                <Card className="!flex !h-[125px] items-center !top-0 !bottom-0 m-auto justify-center px-2">
+                <Card className="!flex !h-[125px] items-center !top-0 max-sm:!w-[200px] !bottom-0 m-auto justify-center px-2">
                     <a href="https://www.icg.ch/" target="_blank">
                         <img src="/img/icg.png" alt="" className={'max-w-[150px]'}/>
                     </a>
                 </Card>
 
-                <Card className="!flex items-center m-auto justify-center !h-[125px] px-2">
+                <Card className="!flex items-center m-auto justify-center max-sm:!w-[200px] !h-[125px] px-2">
                     <a href="https://nativeops.ch/" target="_blank">
                         <img src="/img/nativeops.svg" alt=""  className={'w-[100px]'}/>
                     </a>
                 </Card>
 
-                <Card className="!flex items-center flex-grow !h-full !top-0 !bottom-0 !h-[125px] !absolute align-center justify-center !max-w-fit px-2">
+                <Card className="!flex items-center flex-grow !h-full !h-[125px] align-center justify-center mx-auto w-fit px-2">
                     <a href="https://www.shaw-systems.co.uk/" target="_blank">
-                        <img src="/img/shawsystems.png" alt="" className={'max-w-[200px]'}/>
+                        <img src="/img/shawsystems.png" alt="" className={' max-sm:max-w-[150px] max-w-[200px]'}/>
                     </a>
                 </Card>
 
-                <Card className="!flex justify-content-center !h-[125px] m-auto h-[125px] justify-center w-fit px-2">
+                <Card className="!flex justify-content-center !h-[125px] max-sm:!w-[200px]  m-auto h-[125px] justify-center w-fit px-2">
                     <a className="" href="https://www.daenzer-consulting.ch/" target="_blank">
                         <img src="/img/danzer-logo.png" alt="" className={'max-w-[150px]'}/>
                     </a>
                 </Card>
 
-                 <Card className="!flex !h-[125px] items-center m-auto justify-center px-2">
+                 <Card className="!flex !h-[125px] max-sm:!w-[200px] items-center m-auto max-sm:!w-[70%] justify-center px-2">
                     <a href="https://www.icg.ch/" target="_blank">
                         <img src="/img/icg.png" alt="" className={'max-w-[150px]'}/>
                     </a>
                 </Card>
 
-                <Card className="!flex items-center h-full m-auto justify-center !h-[125px] px-2">
+                <Card className="!flex items-center h-full m-auto max-sm:!w-[200px] justify-center !h-[125px] px-2">
                     <a href="https://nativeops.ch/" target="_blank">
                         <img src="/img/nativeops.svg" alt=""  className={'w-[100px]'}/>
                     </a>
                 </Card>
 
-                <Card className="!flex items-center flex-grow !h-[125px] !top-0 !bottom-0 !absolute align-center justify-center !max-w-fit px-2">
+                <Card className="!flex items-center flex-grow max-sm:!w-[200px] !h-[125px] align-center mx-auto justify-center w-fit px-2">
                     <a href="https://www.shaw-systems.co.uk/" target="_blank">
                         <img src="/img/shawsystems.png" alt="" className={'max-w-[200px]'}/>
                     </a>
                 </Card>
 
-                <Card className="!flex justify-content-center h-full m-auto !h-[125px] justify-center w-fit px-2">
+                <Card className="!flex justify-content-center max-sm:!w-[200px]  h-full m-auto !h-[125px] justify-center w-fit px-2">
                     <a className="" href="https://www.daenzer-consulting.ch/" target="_blank">
                         <img src="/img/danzer-logo.png" alt="" className={'max-w-[150px]'}/>
                     </a>
