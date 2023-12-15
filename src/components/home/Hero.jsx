67da3cd1e9c44487d37f01025d05797ee623d15d
Tim Tran <tim.tran.201@gmail.com>
2023-12-15 20:35:42 +0700

    update

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -5,13 +5,13 @@ import {faArrowRight} from "@fortawesome/free-solid-svg-icons";
 export default function () {
     return (
         <section className="banner-section-two overflow-x-clip">
-            <div className="image-layer" style={{backgroundImage: `url(/img/5.png)`}}></div>
+            <div className="image-layer !bg-repeat" style={{backgroundImage: `url(/img/5.png)`}}></div>
             <div className="auto-container">
                 <div className="clearfix">
                     <div className="content">
-                        <h1>Bring your <br/> software product to market, <span>fast</span></h1>
+                        <h1>Software for modern businesses</h1>
                         <div className="text">
-                            Make your software product vision a reality<br/> with our team of experts.
+                          Innovate and digitize your business processes<br/> with our team of experts.
                         </div>
                         <a href="/#contact"
                            onClick={e => scroll(e, 'contact')}
--- src/components/home/Services.jsx
+++ src/components/home/Services.jsx
@@ -1,122 +1,63 @@
-export default function () {
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faMobile, faCartShopping, faDisplay, faSliders, faPalette, faChartSimple} from "@fortawesome/free-solid-svg-icons";
+
+const services = [
+    {
+        icon: faChartSimple,
+        title: 'Custom Software Development',
+        text: 'We create bespoke software that drives success and innovation in your business.',
+        // className: `bg-gradient-to-r from-[#eb2f5b] to-[#ff8364]`
+    },
+    {
+        icon: faDisplay,
+        title: 'Web Development',
+        text: 'We build beautiful websites and web applications that are scalable, secure, and easy to maintain.'
+    },
+    {
+        icon: faSliders,
+        title: 'ERP Implementation',
+        text: 'We provide comprehensive solutions for integrating and managing your enterprise\'s resources effectively. '
+    },
+    {
+        icon: faMobile,
+        title: 'Mobile Development',
+        text: 'We build dynamic, user-friendly mobile applications tailored to your business needs.'
+    },
+    {
+        icon: faCartShopping,
+        title: 'E-commerce Development',
+        text: 'From custom storefronts to integrated payment systems, we empower your business to thrive in the digital marketplace.'
+    },
+    {
+        icon: faPalette,
+        title: 'UI/UX Design',
+        text: 'We focus on crafting visually stunning and intuitively navigable interfaces, ensuring a delightful user experience. '
+    },
+]
+
+
+export default function Services() {
     return (
-        <section className="services-carousel-section">
-            <div className="outer-container">
-                <div className="big-title">Services</div>
-                <div className="five-item-carousel owl-carousel owl-theme">
-
-                    
-                    <div className="services-block-three">
-                        <div className="inner-box">
-                            <div className="icon-box">
-                                <span className="icon flaticon-target-1"></span>
-                            </div>
-                            <h5><a href="services.html">Market <br/> Research</a></h5>
-                            <div className="text">We’re full service which means...</div>
-                        </div>
-                    </div>
-
-                    
-                    <div className="services-block-three">
-                        <div className="inner-box">
-                            <div className="icon-box">
-                                <span className="icon flaticon-line-chart-1"></span>
-                            </div>
-                            <h5><a href="services.html">Web <br/> Development</a></h5>
-                            <div className="text">We’re full service which means...</div>
-                        </div>
-                    </div>
-
-                    
-                    <div className="services-block-three">
-                        <div className="inner-box">
-                            <div className="icon-box">
-                                <span className="icon flaticon-growth-1"></span>
-                            </div>
-                            <h5><a href="services.html">Strategy & <br/> Planning</a></h5>
-                            <div className="text">We’re full service which means...</div>
-                        </div>
-                    </div>
-
-                    
-                    <div className="services-block-three">
-                        <div className="inner-box">
-                            <div className="icon-box">
-                                <span className="icon flaticon-supermarket"></span>
-                            </div>
-                            <h5><a href="services.html">Growth <br/> Tracking</a></h5>
-                            <div className="text">We’re full service which means...</div>
-                        </div>
-                    </div>
-
-                    
-                    <div className="services-block-three">
-                        <div className="inner-box">
-                            <div className="icon-box">
-                                <span className="icon flaticon-monitor-1"></span>
-                            </div>
-                            <h5><a href="services.html">Enterprise <br/> Consulting</a></h5>
-                            <div className="text">We’re full service which means...</div>
-                        </div>
-                    </div>
-
-                    
-                    <div className="services-block-three">
-                        <div className="inner-box">
-                            <div className="icon-box">
-                                <span className="icon flaticon-target-1"></span>
-                            </div>
-                            <h5><a href="services.html">Market <br/> Research</a></h5>
-                            <div className="text">We’re full service which means...</div>
-                        </div>
-                    </div>
-
-                    
-                    <div className="services-block-three">
-                        <div className="inner-box">
-                            <div className="icon-box">
-                                <span className="icon flaticon-line-chart-1"></span>
-                            </div>
-                            <h5><a href="services.html">Web <br/> Development</a></h5>
-                            <div className="text">We’re full service which means...</div>
-                        </div>
-                    </div>
-
-                    
-                    <div className="services-block-three">
-                        <div className="inner-box">
-                            <div className="icon-box">
-                                <span className="icon flaticon-growth-1"></span>
-                            </div>
-                            <h5><a href="services.html">Strategy & <br/> Planning</a></h5>
-                            <div className="text">We’re full service which means...</div>
-                        </div>
-                    </div>
-
-                    
-                    <div className="services-block-three">
-                        <div className="inner-box">
-                            <div className="icon-box">
-                                <span className="icon flaticon-supermarket"></span>
-                            </div>
-                            <h5><a href="services.html">Growth <br/> Tracking</a></h5>
-                            <div className="text">We’re full service which means...</div>
-                        </div>
-                    </div>
-
-                    
-                    <div className="services-block-three">
-                        <div className="inner-box">
-                            <div className="icon-box">
-                                <span className="icon flaticon-monitor-1"></span>
-                            </div>
-                            <h5><a href="services.html">Enterprise <br/> Consulting</a></h5>
-                            <div className="text">We’re full service which means...</div>
-                        </div>
-                    </div>
-
+        <section className="services-section max-w-screen-lg mx-auto">
+            <div className="auto-container">
+                <div className="sec-title centered">
+                    {/*<div className="title">what we do</div>*/}
+                    <h2>Our services</h2>
                 </div>
+
+                <div className={`flex flex-wrap gap-4 justify-center`}>
+                    {services.map((service, index) =>
+                        <div
+                            className={`shadow-sm rounded-lg bg-white border border-gray-300 w-[300px] h-[220px] p-4 flex flex-col gap-2 items-start ${service.className}`}
+                            key={index}>
+                            <FontAwesomeIcon icon={service.icon} className={`text-3xl text-[#eb2f5b]`}/>
+                            <div className={`font-bold text-lg text-black`}>{service.title}</div>
+                            <div>{service.text}</div>
+                        </div>
+                    )}
+                </div>
+
             </div>
         </section>
-)
+    )
 }
