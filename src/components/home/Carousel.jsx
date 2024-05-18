78f2ff60260bc4e3f4062cc0666eda85b7a77c74
lytrann <mikiritrann@gmail.com>
2024-05-19 01:40:57 +0700

    fix carousel

---
+++ src/components/home/Carousel.jsx
@@ -0,0 +1,161 @@
+import {useEffect, useRef, useState} from 'react';
+import {faAngleLeft, faAngleRight} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {Link} from "react-router-dom";
+import {useHash} from "@mantine/hooks"
+// import {useTranslation} from "react-i18next";
+import {Card} from "@deepsel/lake-ui";
+import '../../assets/css/scrollbar.css'
+
+
+export default function Carousel() {
+    let [position, setPosition] = useState(0)
+
+    let clients = [
+        {
+            id: "icg",
+            image: "/img/icg.png",
+            path: "https://www.icg.ch/",
+        },
+        {
+            id: "native-ops",
+            image: "/img/nativeops.svg",
+            path: "https://nativeops.ch/",
+        },
+
+        {
+            id: "daenzer",
+            image: "/img/danzer-logo.png",
+            path: "https://www.daenzer-consulting.ch/",
+        },
+        {
+            id: "sekops",
+            image: "/img/sekops.svg",
+            path: "https://bernardgutermann.wixsite.com/sops",
+        },
+        {
+            id: "shaw-systems",
+            image: "/img/shawsystems.png",
+            path: "https://www.shaw-systems.co.uk/",
+        },
+        {
+            id: "icg",
+            image: "/img/icg.png",
+            path: "https://www.icg.ch/",
+        },
+        {
+            id: "native-ops",
+            image: "/img/nativeops.svg",
+            path: "https://nativeops.ch/",
+        },
+
+        {
+            id: "daenzer",
+            image: "/img/danzer-logo.png",
+            path: "https://www.daenzer-consulting.ch/",
+        },
+        {
+            id: "sekops",
+            image: "/img/sekops.svg",
+            path: "https://bernardgutermann.wixsite.com/sops",
+        },
+        {
+            id: "shaw-systems",
+            image: "/img/shawsystems.png",
+            path: "https://www.shaw-systems.co.uk/",
+        },
+        {
+            id: "icg",
+            image: "/img/icg.png",
+            path: "https://www.icg.ch/",
+        },
+        {
+            id: "native-ops",
+            image: "/img/nativeops.svg",
+            path: "https://nativeops.ch/",
+        },
+
+        {
+            id: "daenzer",
+            image: "/img/danzer-logo.png",
+            path: "https://www.daenzer-consulting.ch/",
+        },
+        {
+            id: "sekops",
+            image: "/img/sekops.svg",
+            path: "https://bernardgutermann.wixsite.com/sops",
+        },
+        {
+            id: "shaw-systems",
+            image: "/img/shawsystems.png",
+            path: "https://www.shaw-systems.co.uk/",
+        },
+    ];
+
+
+    const ref = useRef(null);
+    useEffect(() => {
+        return () => {
+            if (ref) {
+                ref.current.scrollTo(1500, 0)
+            }
+        };
+    }, [ref]);
+
+
+    useEffect(() => {
+            console.log(position)
+            if (Math.abs(position % 5) === 0) {
+                if (ref) {
+                    function scroll() {
+                        ref.current.scrollTo(1500, 0)
+                    }
+                    setTimeout(scroll, 600)
+                }
+            }
+        }
+        ,
+        [position]
+    )
+    ;
+    return (
+        <main className="relative mx-auto pt-2">
+            <div
+                className={'max-md:w-full max-lg:w-[750px] w-[1200px] flex justify-items-center justify-center mx-auto'}>
+                <button className={'w-[100px] max-md:w-[50px] cursor-pointer bg-white'} onClick={() => {
+                    ref.current.scrollBy({left: -300, behavior: 'smooth'})
+                    setPosition(position - 1)
+                }}>
+                    <FontAwesomeIcon icon={faAngleLeft} size={'2xl'}/>
+                </button>
+                <div className={'w-[900px]  overflow-x-hidden'}>
+                    <div
+                        className=" hide-scrollbar flex flex-row overflow-x-scroll gap-[100px] mx-[50px]  text-center"
+                        ref={ref}>
+                        {clients?.map((client, index) => (
+                            <Link key={index}
+                                  to={client.path}
+                                // onClick={() => setHash(client.id)}
+                            >
+                                <Card
+                                    className="!flex content-center w-[200px] h-full m-auto !h-[125px] justify-center w-fit px-2">
+                                    <a className="w-fit my-auto" href="https://www.daenzer-consulting.ch/"
+                                       target="_blank">
+                                        <img src={client.image} alt="" className={'max-w-[150px] my-auto'}/>
+                                    </a>
+                                </Card>
+                            </Link>
+                        ))}
+                    </div>
+                </div>
+                <button className={'w-[100px] cursor-pointer max-md:w-[50px] bg-white'} onClick={() => {
+                    ref.current.scrollBy({left: 300, behavior: 'smooth'})
+                    setPosition(position + 1)
+                }}>
+                    <FontAwesomeIcon icon={faAngleRight} size={'2xl'}/>
+                </button>
+            </div>
+
+        </main>
+    );
+}
--- src/components/home/Clients.jsx
+++ src/components/home/Clients.jsx
@@ -1,10 +1,11 @@
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faChevronLeft, faChevronRight} from "@fortawesome/free-solid-svg-icons";
-import Slider from "react-slick";
+// import Slider from "react-slick";
 import {Card} from "@deepsel/lake-ui";
 import {useEffect, useState} from "react";
+import Carousel from "./Carousel";
 
 
 export default function () {
@@ -13,7 +14,6 @@ export default function () {
     useEffect(() => {
         return () => {
             const innerWidth = window.innerWidth;
-            console.log(innerWidth);
             if (innerWidth < 641) {
                 setSlideNumber(1)
             } else if (innerWidth < 769) {
@@ -21,7 +21,6 @@ export default function () {
             } else if (innerWidth < 1025) {
                 setSlideNumber(3)
             } else setSlideNumber(4)
-            console.log(slideNumber);
         };
     }, [innerWidth]);
 
@@ -54,55 +53,57 @@ export default function () {
                 <div className="title">trusted by</div>
                 <h2>Our clients</h2>
             </div>
-            <Slider className={'w-[80%] mx-auto'} {...settings}>
-                <Card className="!flex !h-[125px] items-center !top-0 max-sm:!w-[200px] !bottom-0 m-auto justify-center px-2">
-                    <a href="https://www.icg.ch/" target="_blank">
-                        <img src="/img/icg.png" alt="" className={'max-w-[150px]'}/>
-                    </a>
-                </Card>
+            {/*<Slider className={'w-[80%] mx-auto'} {...settings}>*/}
+            {/*    <Card className="!flex !h-[125px] items-center !top-0 max-sm:!w-[200px] !bottom-0 m-auto justify-center px-2">*/}
+            {/*        <a href="https://www.icg.ch/" target="_blank">*/}
+            {/*            <img src="/img/icg.png" alt="" className={'max-w-[150px]'}/>*/}
+            {/*        </a>*/}
+            {/*    </Card>*/}
 
-                <Card className="!flex items-center m-auto justify-center max-sm:!w-[200px] !h-[125px] px-2">
-                    <a href="https://nativeops.ch/" target="_blank">
-                        <img src="/img/nativeops.svg" alt=""  className={'w-[100px]'}/>
-                    </a>
-                </Card>
+            {/*    <Card className="!flex items-center m-auto justify-center max-sm:!w-[200px] !h-[125px] px-2">*/}
+            {/*        <a href="https://nativeops.ch/" target="_blank">*/}
+            {/*            <img src="/img/nativeops.svg" alt=""  className={'w-[100px]'}/>*/}
+            {/*        </a>*/}
+            {/*    </Card>*/}
 
-                <Card className="!flex items-center flex-grow !h-[125px] align-center justify-center mx-auto w-fit px-2">
-                    <a href="https://www.shaw-systems.co.uk/" target="_blank">
-                        <img src="/img/shawsystems.png" alt="" className={' max-sm:max-w-[150px] max-w-[200px]'}/>
-                    </a>
-                </Card>
+            {/*    <Card className="!flex items-center flex-grow !h-[125px] align-center justify-center mx-auto w-fit px-2">*/}
+            {/*        <a href="https://www.shaw-systems.co.uk/" target="_blank">*/}
+            {/*            <img src="/img/shawsystems.png" alt="" className={' max-sm:max-w-[150px] max-w-[200px]'}/>*/}
+            {/*        </a>*/}
+            {/*    </Card>*/}
 
-                <Card className="!flex justify-content-center !h-[125px] max-sm:!w-[200px]  m-auto h-[125px] justify-center w-fit px-2">
-                    <a className="" href="https://www.daenzer-consulting.ch/" target="_blank">
-                        <img src="/img/danzer-logo.png" alt="" className={'max-w-[150px]'}/>
-                    </a>
-                </Card>
+            {/*    <Card className="!flex justify-content-center !h-[125px] max-sm:!w-[200px]  m-auto h-[125px] justify-center w-fit px-2">*/}
+            {/*        <a className="" href="https://www.daenzer-consulting.ch/" target="_blank">*/}
+            {/*            <img src="/img/danzer-logo.png" alt="" className={'max-w-[150px]'}/>*/}
+            {/*        </a>*/}
+            {/*    </Card>*/}
 
-                 <Card className="!flex !h-[125px] max-sm:!w-[200px] items-center m-auto max-sm:!w-[70%] justify-center px-2">
-                    <a href="https://www.icg.ch/" target="_blank">
-                        <img src="/img/icg.png" alt="" className={'max-w-[150px]'}/>
-                    </a>
-                </Card>
+            {/*     <Card className="!flex !h-[125px] max-sm:!w-[200px] items-center m-auto max-sm:!w-[70%] justify-center px-2">*/}
+            {/*        <a href="https://www.icg.ch/" target="_blank">*/}
+            {/*            <img src="/img/icg.png" alt="" className={'max-w-[150px]'}/>*/}
+            {/*        </a>*/}
+            {/*    </Card>*/}
 
-                <Card className="!flex items-center h-full m-auto max-sm:!w-[200px] justify-center !h-[125px] px-2">
-                    <a href="https://nativeops.ch/" target="_blank">
-                        <img src="/img/nativeops.svg" alt=""  className={'w-[100px]'}/>
-                    </a>
-                </Card>
+            {/*    <Card className="!flex items-center h-full m-auto max-sm:!w-[200px] justify-center !h-[125px] px-2">*/}
+            {/*        <a href="https://nativeops.ch/" target="_blank">*/}
+            {/*            <img src="/img/nativeops.svg" alt=""  className={'w-[100px]'}/>*/}
+            {/*        </a>*/}
+            {/*    </Card>*/}
 
-                <Card className="!flex items-center flex-grow max-sm:!w-[200px] !h-[125px] align-center mx-auto justify-center w-fit px-2">
-                    <a href="https://www.shaw-systems.co.uk/" target="_blank">
-                        <img src="/img/shawsystems.png" alt="" className={'max-w-[200px]'}/>
-                    </a>
-                </Card>
+            {/*    <Card className="!flex items-center flex-grow max-sm:!w-[200px] !h-[125px] align-center mx-auto justify-center w-fit px-2">*/}
+            {/*        <a href="https://www.shaw-systems.co.uk/" target="_blank">*/}
+            {/*            <img src="/img/shawsystems.png" alt="" className={'max-w-[200px]'}/>*/}
+            {/*        </a>*/}
+            {/*    </Card>*/}
 
-                <Card className="!flex justify-content-center max-sm:!w-[200px]  h-full m-auto !h-[125px] justify-center w-fit px-2">
-                    <a className="" href="https://www.daenzer-consulting.ch/" target="_blank">
-                        <img src="/img/danzer-logo.png" alt="" className={'max-w-[150px]'}/>
-                    </a>
-                </Card>
-            </Slider>
+                {/*<Card className="!flex justify-content-center max-sm:!w-[200px]  h-full m-auto !h-[125px] justify-center w-fit px-2">*/}
+                {/*    <a className="" href="https://www.daenzer-consulting.ch/" target="_blank">*/}
+                {/*        <img src="/img/danzer-logo.png" alt="" className={'max-w-[150px]'}/>*/}
+                {/*    </a>*/}
+                {/*</Card>*/}
+            {/*</Slider>*/}
+            <Carousel/>
+
         </section>
     )
 }
